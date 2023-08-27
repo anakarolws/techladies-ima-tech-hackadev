9922b07131cbeacbe8cfb1238e50dfbaa309cb2b
Paulini Duarte <118136588+Paulini86@users.noreply.github.com>
2023-08-27 16:10:19 -0300

    Update main.dart

--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -5,10 +5,13 @@ import 'package:ecommerce/pages/smartphone_page.dart';
 import 'package:ecommerce/pages/smarttv.dart';
 import 'package:flutter/material.dart';
 
+
 import 'pages/cart_page.dart';
 import 'pages/home_page.dart';
 // ignore: duplicate_import
 import 'pages/minha_conta_page.dart';
+// ignore: unused_import
+import 'pages/favoritos.dart';
 
 
 
@@ -37,9 +40,12 @@ class MyApp extends StatelessWidget {
         "/smartphonePage": (context) => SmartphonePage(),
         "/smartTv": (context) => SmartTv(),
         "/configuracoesPage": (context) => const ConfiguracoesPage(),
+        "favoritosPage": (context) => const Favoritos(),
+        
         
       },
     );
   }
 }
+
 
