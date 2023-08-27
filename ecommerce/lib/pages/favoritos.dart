e667ef216749234d7be947349f666ffe58ba83d0
Paulini Duarte <118136588+Paulini86@users.noreply.github.com>
2023-08-27 16:17:32 -0300

    Update favoritos.dart

--- ecommerce/lib/pages/favoritos.dart
+++ ecommerce/lib/pages/favoritos.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class Favoritos extends StatelessWidget {
+  const Favoritos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favoritos', style: TextStyle(color: Colors.purple),),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.grey),
+      ),
+      body: const Center(
+        child: Text(
+          'Resultados para: Favoritos',
+          style: TextStyle(
+            color: Colors.grey,
+            decoration: TextDecoration.none,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
