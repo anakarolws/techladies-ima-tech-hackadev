82044d5faaf2a23d8ed1bf297e49c27bed65bbfa
Grazielly Cunha <grazielly_cunha@hotmail.com>
2023-08-28 04:33:45 -0300

    Alteração layout EletronicosPage

--- ecommerce/lib/pages/eletronicos_page.dart
+++ ecommerce/lib/pages/eletronicos_page.dart
@@ -1,180 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 
+import '../model/dados_produtos.dart';
 
 class EletronicosPage extends StatelessWidget {
-  EletronicosPage({super.key});
-
-  final List<Map<String, dynamic>> products = [
-      {
-      "image": "assets/img/category/eletroeletronicos/fone01.webp",
-      "title": "Fone de Ouvido",
-      "description": "Fone de ouvido sem fio ",
-      "price": 60.00,
-      "discount": 30
-    },
-    {
-      "image": "assets/img/category/eletroeletronicos/fone02.webp",
-      "title": "Fone Gamer",
-      "description": "Fone de ouvido gamer",
-      "price": 120.00,
-      "discount": 20
-    },
-    {
-      "image": "assets/img/category/eletroeletronicos/fone03.webp",
-      "title": "Fone Bluetooth",
-      "description": "Fone com bateria de longa duração",
-      "price": 80.00,
-      "discount": 10
-    },
-    {
-      "image": "assets/img/category/eletroeletronicos/fone04.webp",
-      "title": "Fone Esportivo",
-      "description": "Fone esportivo à prova d'água",
-      "price": 100.00,
-      "discount": 15
-    }
-  ];
+  const EletronicosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-        backgroundColor: Color.fromRGBO(109, 68, 166, 1),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(20),
-          child: Container(
-            color: Color.fromRGBO(109, 68, 166, 1),
-            padding: const EdgeInsets.all(25),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () {
-                    //retorna para a tela anterior
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.arrow_back,
-                    size: 25,
-                    color: Colors.white,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text(
-                    "Eletrônicos",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+        appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Color.fromRGBO(109, 68, 166, 1),
+        ),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            "Eletrônicos",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(109, 68, 166, 1),
             ),
           ),
         ),
       ),
     
-      body: GridView.count(
-      childAspectRatio: 0.68,
-      physics: NeverScrollableScrollPhysics(),
-      crossAxisCount: 2,
-      shrinkWrap: true,
-      children: products.map((product) => Container(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(156, 131, 192, 0.10),
-              borderRadius: BorderRadius.circular(20),
+       body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 0.78),
+        itemCount: eletronicos.length,
+        itemBuilder: (context, index) {
+          return Card(
+            
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(color: Color.fromRGBO(109, 68, 166, 1), width: 1),
+              borderRadius: BorderRadius.circular(10.0),
             ),
-            child: Column(
-              children: [
-                //linha da porcentagem de desconto e do botão de curtir
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: const Color.fromRGBO(109, 68, 166, 1),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text(
-                        "-${product['discount']}%",
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    //botão favoritos
-                    const LikeButton(
-                     animationDuration: Duration(seconds: 0)
-                    )
-                  ],
-                ),
-
-                //imagem do produto inclusa em container card
-                Expanded(
-                  child: Material(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Image.asset(
-                          product['image'],
-                          height: 120,
-                          width: 120,
-                        ),
+            
+            child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(109, 68, 166, 1),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "-${eletronicos[index].discount}%",
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
-
-                //titulo do produto em card
-               Container(
-    padding: const EdgeInsets.only(bottom: 8),
-    child: Text(
-      product['title'],
-      style: const TextStyle(
-        fontSize: 15,
-        color: Color.fromRGBO(109, 68, 166, 1),
-        fontWeight: FontWeight.bold,
-      ),
-    )),
-
-//descrição do produto de card
-Container(
-  alignment: Alignment.centerLeft,
-  child: Text(
-    product['description'],
-    style: const TextStyle(
-        fontSize: 12, color: Color.fromRGBO(109, 68, 166, 1)),
-  ),
-),
-
-Padding(
-    padding: EdgeInsets.symmetric(vertical: 10),
-    child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "R\$${product['price']}",
-            style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(109, 68, 166, 1)),
-          ),
-          const Icon(
-            Icons.shopping_cart_checkout,
-            color: Color.fromRGBO(109, 68, 166, 1),
-          ),
-        ])),
-              ],
+                  //botão favoritos
+                    const LikeButton(
+                     animationDuration: Duration(seconds: 0)
+                    )
+                ],
+              ),
             ),
-          )).toList(),
-    ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Image.asset(
+                  eletronicos[index].image!,
+                  height: 120,
+                  width: 140,
+                ),
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(eletronicos[index].title!,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        color: Color.fromRGBO(109, 68, 166, 1),
+                        fontWeight: FontWeight.bold))),
+            Container(
+              alignment: Alignment.center,
+              child: Text(eletronicos[index].description!,
+                  style: const TextStyle(
+                      fontSize: 12, color: Color.fromRGBO(109, 68, 166, 1))),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("R\$${eletronicos[index].price}",
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(109, 68, 166, 1))),
+                      const Icon(Icons.shopping_cart_checkout,
+                          color: Color.fromRGBO(109, 68, 166, 1)),
+                    ])),
+          ]));
+        },
+      ),
     );
   }
 }
