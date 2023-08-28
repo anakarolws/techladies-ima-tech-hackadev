ba33884c24cf97f120220508b1987e24c6e506e0
Grazielly Cunha <grazielly_cunha@hotmail.com>
2023-08-28 04:29:25 -0300

    Alteração layout SmartPhonePage

--- ecommerce/lib/pages/smartphone_page.dart
+++ ecommerce/lib/pages/smartphone_page.dart
@@ -1,181 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 
-class SmartphonePage extends StatelessWidget {
-  SmartphonePage({super.key});
+import '../model/dados_produtos.dart';
 
-  final List<Map<String, dynamic>> products = [
-    {
-      "image": "assets/img/category/smartphones/01.webp",
-      "title": "IPhone",
-      "description": "O melhor do Iphone",
-      "price": 6000.00,
-      "discount": 30
-    },
-    {
-      "image": "assets/img/category/smartphones/02.webp",
-       "title": "IPhone",
-      "description": "O segundo melhor do Iphone",
-      "price": 5000.00,
-      "discount": 20
-    },
-    {
-      "image": "assets/img/category/smartphones/03.webp",
-      "title": "Xiaomi",
-      "description": "O melhor do Xiami",
-      "price": 2000.00,
-      "discount": 10
-    },
-    {
-      "image": "assets/img/category/smartphones/04.webp",
-      "title": "Xiaomi",
-      "description": "Mais barato e melhor",
-      "price": 2500.00,
-      "discount": 15
-    }
-  ];
+class SmartphonePage extends StatelessWidget {
+  const SmartphonePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(109, 68, 166, 1),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(20),
-          child: Container(
-            color: Color.fromRGBO(109, 68, 166, 1),
-            padding: const EdgeInsets.all(25),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () {
-                    //retorna para a tela anterior
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.arrow_back,
-                    size: 25,
-                    color: Colors.white,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text(
-                    "Smartphones",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+       appBar: AppBar(
+        backgroundColor: Colors.white,
+        
+        iconTheme: const IconThemeData(
+          color: Color.fromRGBO(109, 68, 166, 1),
+          
+        ),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            "Smartphones",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(109, 68, 166, 1),
             ),
           ),
         ),
       ),
-      body: GridView.count(
-        childAspectRatio: 0.68,
-        physics: NeverScrollableScrollPhysics(),
-        crossAxisCount: 2,
-        shrinkWrap: true,
-        children: products
-            .map((product) => Container(
-                  padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(156, 131, 192, 0.10),
-                    borderRadius: BorderRadius.circular(20),
+
+
+        body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 0.78),
+        itemCount: smartphones.length,
+        itemBuilder: (context, index) {
+          
+          return Card(
+           
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(color: Color.fromRGBO(109, 68, 166, 1), width: 1),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            
+            child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(109, 68, 166, 1),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "-${smartphones[index].discount}%",
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  child: Column(
-                    children: [
-                      //linha da porcentagem de desconto e do botão de curtir
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: const Color.fromRGBO(109, 68, 166, 1),
-                                borderRadius: BorderRadius.circular(20)),
-                            child: Text(
-                              "-${product['discount']}%",
-                              style: const TextStyle(
-                                fontSize: 12,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          //botão favoritos
-                    const LikeButton(
+                   const LikeButton(
                      animationDuration: Duration(seconds: 0)
-                    )
-                        ],
-                      ),
-
-                      //imagem do produto inclusa em container card
-                      Expanded(
-                        child: Material(
-                          child: InkWell(
-                            onTap: () {},
-                            child: Container(
-                              margin: const EdgeInsets.all(10),
-                              child: Image.asset(
-                                product['image'],
-                                height: 120,
-                                width: 120,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-
-                      //titulo do produto em card
-                      Container(
-                          padding: const EdgeInsets.only(bottom: 8),
-                          child: Text(
-                            product['title'],
-                            style: const TextStyle(
-                              fontSize: 15,
-                              color: Color.fromRGBO(109, 68, 166, 1),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
-
-//descrição do produto de card
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          product['description'],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Image.asset(
+                  smartphones[index].image!,
+                  height: 120,
+                  width: 120,
+                ),
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(smartphones[index].title!,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        color: Color.fromRGBO(109, 68, 166, 1),
+                        fontWeight: FontWeight.bold))),
+            Container(
+              alignment: Alignment.center,
+              child: Text(smartphones[index].description!,
+                  style: const TextStyle(
+                      fontSize: 12, color: Color.fromRGBO(109, 68, 166, 1))),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("R\$${smartphones[index].price}",
                           style: const TextStyle(
-                              fontSize: 12,
-                              color: Color.fromRGBO(109, 68, 166, 1)),
-                        ),
-                      ),
-
-                      Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "R\$${product['price']}",
-                                  style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromRGBO(109, 68, 166, 1)),
-                                ),
-                                const Icon(
-                                  Icons.shopping_cart_checkout,
-                                  color: Color.fromRGBO(109, 68, 166, 1),
-                                ),
-                              ])),
-                    ],
-                  ),
-                ))
-            .toList(),
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(109, 68, 166, 1))),
+                      const Icon(Icons.shopping_cart_checkout,
+                          color: Color.fromRGBO(109, 68, 166, 1)),
+                    ])),
+          ]));
+        },
       ),
     );
   }
