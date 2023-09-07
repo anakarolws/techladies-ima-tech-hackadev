3f93761362efd0a9e64d76c50c3b85f1f9c7c2e7
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-09-07 12:27:56 -0300

    Update eletronicos_page.dart

--- ecommerce/lib/pages/eletronicos_page.dart
+++ ecommerce/lib/pages/eletronicos_page.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/model/dados_produtos.dart';
+import 'package:ecommerce/model/eletronicos.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 
@@ -26,81 +27,125 @@ class EletronicosPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     const String tagImage = 'hero-eletronicos';
 
     DetalhesPage detalhesPage = DetalhesPage();
 
     return Scaffold(
-      appBar: CustomAppBar(title: title),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 0.78),
-        itemCount: eletronicos.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () => detalhesPage.detalhesCategoria(context, eletronicos, index, tagImage),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(
-                    color: Color.fromRGBO(109, 68, 166, 1), width: 1),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Column(children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: const Color.fromRGBO(109, 68, 166, 1),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          "-${eletronicos[index].discount}%",
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+        appBar: CustomAppBar(title: title),
+        body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 0.78),
+            itemCount: eletronicos.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                  onTap: () => detalhesPage.detalhesCategoria(
+                      context, eletronicos, index, tagImage),
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                            color: Color.fromRGBO(109, 68, 166, 1), width: 1),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 7),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    color:
+                                        const Color.fromRGBO(109, 68, 166, 1),
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Text(
+                                  "-${eletronicos[index].discount}%",
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              //botão favoritos
+                              const LikeButton(
+                                  animationDuration: Duration(seconds: 0))
+                            ],
                           ),
                         ),
-                      ),
-                      //botão favoritos
-                      const LikeButton(animationDuration: Duration(seconds: 0))
-                    ],
-                  ),
-                ),
-               Expanded(
-                  child: HeroImage(
-                      tag: '$tagImage-$index', image: eletronicos[index].image!),
-                ),
-                Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(eletronicos[index].title!,
-                        style: const TextStyle(
-                            fontSize: 15,
-                            color: Color.fromRGBO(109, 68, 166, 1),
-                            fontWeight: FontWeight.bold))),
-                Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("R\$${eletronicos[index].price}",
-                              style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color.fromRGBO(109, 68, 166, 1))),
-                          const Icon(Icons.shopping_cart_checkout,
-                              color: Color.fromRGBO(109, 68, 166, 1)),
-                        ])),
-              ]))
-          );
-        },
-      ),
-    );
+                        Expanded(
+                          child: HeroImage(
+                              tag: '$tagImage-$index',
+                              image: eletronicos[index].image!),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(bottom: 8),
+                            child: Text(eletronicos[index].title!,
+                                style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Color.fromRGBO(109, 68, 166, 1),
+                                    fontWeight: FontWeight.bold))),
+                        Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 5),
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("R\$${eletronicos[index].price}",
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              Color.fromRGBO(109, 68, 166, 1))), 
+                                  IconButton(
+                                    icon: Icon(Icons.credit_card),
+                                    onPressed: () {
+                                      // Exibe o modal com as informações do pagamento
+                                      showModalBottomSheet(
+                                          context: context,
+                                          builder: (context) {
+                                            final Eletronicos =
+                                                eletronicos[index];
+                                            final priceWithDiscount =
+                                                Eletronicos.price! * 0.9;
+                                            return Column (children: [
+                                              ListTile(
+                                                title: Text(
+                                                    'Preço à vista com desconto:'),
+                                                subtitle: Text(
+                                                    'R\$ ${priceWithDiscount.toStringAsFixed(2)}'),
+                                              ),
+                                              ListTile(
+                                                  title: Text(
+                                                      'Opções de Parcelamento:'),
+                                                  subtitle: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        for (int i = 1;
+                                                            i <= 6;
+                                                            i++)
+                                                          ListTile(
+                                                            title: Text(
+                                                                '$i x ${calculateParcelValue(priceWithDiscount, i).toStringAsFixed(2)}'),
+                                                          )
+                                                      ]))
+                                            ]);
+                                          });
+                                    },
+                                  )
+                                ]))
+                      ])));
+            }));
+  }
+
+  double calculateParcelValue(double price, int numberOfInstallments) {
+    final interestRate = 0.02; // 2% de juros
+    final totalValue = price * (1 + interestRate);
+    return totalValue / numberOfInstallments;
   }
 }
