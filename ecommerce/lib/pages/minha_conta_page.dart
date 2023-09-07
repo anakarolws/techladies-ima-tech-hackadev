19512126bdcf4c4e86d506dd06f9848717e9f3a9
Renata Rodrigues <105400565+Re-na6ta@users.noreply.github.com>
2023-09-07 15:16:21 -0300

    Update minha_conta_page.dart

--- ecommerce/lib/pages/minha_conta_page.dart
+++ ecommerce/lib/pages/minha_conta_page.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:login/login_controller.dart';
 
-class MinhaContaPage extends StatelessWidget {
-  const MinhaContaPage({super.key});
+class LoginPage extends StatelessWidget {
+  final LoginController _controller = LoginController();
+
+  LoginPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Minha Conta', style: TextStyle(color: Colors.purple),),
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: Colors.grey),
+        title: Text('Login'),
       ),
-      body: const Center(
-        child: Text(
-          'Resultados para: Minha Conta',
-          style: TextStyle(
-            color: Colors.grey,
-            decoration: TextDecoration.none,
-          ),
-          textAlign: TextAlign.center,
+      body: Container(
+        padding: EdgeInsets.all(28),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.people, size: MediaQuery.of(context).size.height * 0.2),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Login',
+              ),
+              onChanged: _controller.setLogin,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Senha',
+              ),
+              obscureText: true,
+              onChanged: _controller.setPass,
+            ),
+            SizedBox(height: 15),
+            ValueListenableBuilder<bool>(
+              valueListenable: _controller.inLoader,
+              builder: (_,inLoader,__) => inLoader
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: () {
+                        _controller.auth().then(
+                          (result) {
+                            if (result) {
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/home');
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content:
+                                      const Text('Falha ao realizar login'),
+                                  duration: const Duration(seconds: 3),
+                                ),
+                              );
+                            }
+                          },
+                        );
+                      },
+                      child: Text('Login'),
+                    ),
+            ),
+          ],
         ),
       ),
     );
