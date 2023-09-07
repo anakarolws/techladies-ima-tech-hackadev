081cada3360772fc9cc3e49f05656e71eeb43f47
Renata Rodrigues <105400565+Re-na6ta@users.noreply.github.com>
2023-09-07 15:19:49 -0300

    Create login_controller.dart

---
+++ ecommerce/lib/pages/login_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class LoginController{
+
+  ValueNotifier<bool>inLoader=ValueNotifier<bool>(false);
+
+  String? _login;
+  setLogin(String value) => _login =value;
+
+
+  String? _pass;
+  setPass(String value) => _pass =value;
+
+  Future<bool>auth() async{
+    inLoader.value = true;
+    await Future.delayed(Duration(seconds: 2));
+     inLoader.value = false;
+  return(_login=='admin'&& _pass=='123');
+
+   
+  }
+}
