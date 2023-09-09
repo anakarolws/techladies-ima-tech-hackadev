f8e16e50c493715fcbad4f6a92980d130e84914f
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-09-09 20:06:35 -0300

    Create botao_favoritos.dart

---
+++ ecommerce/lib/model/botao_favoritos.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+class FavoritosBotao extends StatefulWidget {
+  @override
+  _FavoritosBotaoState createState() => _FavoritosBotaoState();
+}
+
+class _FavoritosBotaoState extends State<FavoritosBotao> {
+  bool isFavorito = false;
+
+  void _mostrarSnackBar(String mensagem) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(mensagem),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isFavorito = !isFavorito;
+                if (isFavorito) {
+                  _mostrarSnackBar('Adicionado aos Favoritos');
+                } else {
+                  _mostrarSnackBar('Removido dos Favoritos');
+                }
+              });
+            },
+            icon: Icon(
+              isFavorito ? Icons.favorite : Icons.favorite_border,
+              color: isFavorito ? Color.fromRGBO(109, 68, 166, 1) : null,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
