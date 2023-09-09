78f136d157f4e95567d9c394530e89528abddef4
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-09-09 20:04:11 -0300

    Update smartphone_page.dart

--- ecommerce/lib/pages/smartphone_page.dart
+++ ecommerce/lib/pages/smartphone_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/model/botao_favoritos.dart';
 import 'package:ecommerce/model/dados_produtos.dart';
 import 'package:ecommerce/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class _SmartPhonePriceState extends State<SmartPhonePrice> {
 }
 
 class SmartphonePage extends StatelessWidget {
-  const SmartphonePage({super.key});
+  const SmartphonePage({Key? key});
   final String title = "Smartphones";
 
  
@@ -29,80 +30,114 @@ class SmartphonePage extends StatelessWidget {
   Widget build(BuildContext context) {
     const String tagImage = 'hero-smartphone';
 
-    DetalhesPage detalhesPage = DetalhesPage();
 
-    return Scaffold(
+     return Scaffold(
       appBar: CustomAppBar(title: title),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 0.78),
+          crossAxisCount: 2,
+          childAspectRatio: 0.78,
+        ),
         itemCount: smartphones.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-             onTap: () => detalhesPage.detalhesCategoria(
-                context, smartphones, index, tagImage),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => DetalhesPage(
+                    categoria: smartphones,
+                    index: index,
+                    tagValor: tagImage,
+                  ),
+                ),
+              );
+            },
             child: Card(
-           
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Color.fromRGBO(109, 68, 166, 1), width: 1),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            
-            child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  color: Color.fromRGBO(109, 68, 166, 1),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
                 children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 7,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(109, 68, 166, 1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            "-${smartphones[index].discount}%",
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        // Botão favoritos
+                        FavoritosBotao(),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: HeroImage(
+                      tag: '$tagImage-$index',
+                      image: smartphones[index].image!,
+                    ),
+                  ),
                   Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(109, 68, 166, 1),
-                        borderRadius: BorderRadius.circular(20)),
+                    padding: const EdgeInsets.only(bottom: 8),
                     child: Text(
-                      "-${smartphones[index].discount}%",
+                      smartphones[index].title!,
                       style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
+                        fontSize: 15,
+                        color: Color.fromRGBO(109, 68, 166, 1),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                   const LikeButton(
-                     animationDuration: Duration(seconds: 0)
-                  )
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 5,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "R\$${smartphones[index].price}",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(109, 68, 166, 1),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.shopping_cart_checkout,
+                          color: Color.fromRGBO(109, 68, 166, 1),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            Expanded(
-              child: HeroImage(
-                      tag: '$tagImage-$index', image: smartphones[index].image!),
-            ),
-            Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(smartphones[index].title!,
-                    style: const TextStyle(
-                        fontSize: 15,
-                        color: Color.fromRGBO(109, 68, 166, 1),
-                        fontWeight: FontWeight.bold))),
-           
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("R\$${smartphones[index].price}",
-                          style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromRGBO(109, 68, 166, 1))),
-                      const Icon(Icons.shopping_cart_checkout,
-                          color: Color.fromRGBO(109, 68, 166, 1)),
-                    ])),
-          ]))
           );
         },
       ),
     );
   }
 }
+
+
