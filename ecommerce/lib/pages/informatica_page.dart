ce1c731988bae7671adff8a64a85a29d9af8fd52
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-09-09 20:02:53 -0300

    Update informatica_page.dart

--- ecommerce/lib/pages/informatica_page.dart
+++ ecommerce/lib/pages/informatica_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/model/botao_favoritos.dart';
 import 'package:ecommerce/model/dados_produtos.dart';
 import 'package:ecommerce/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -21,82 +22,116 @@ class _InformaticaPriceState extends State<InformaticaPrice> {
 }
 
 class InformaticaPage extends StatelessWidget {
-  const InformaticaPage({super.key});
+  const InformaticaPage({Key? key});
   final String titulo = "Informática";
 
   @override
   Widget build(BuildContext context) {
     const String tagImage = 'hero-informatica';
 
-    DetalhesPage detalhesPage = DetalhesPage();
 
-    return Scaffold(
+     return Scaffold(
       appBar: CustomAppBar(title: titulo),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 0.78),
+          crossAxisCount: 2,
+          childAspectRatio: 0.78,
+        ),
         itemCount: informatica.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-            onTap: () => detalhesPage.detalhesCategoria(context, informatica, index, tagImage),
-            child:  Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(
-                    color: Color.fromRGBO(109, 68, 166, 1), width: 1),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Column(children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: const Color.fromRGBO(109, 68, 166, 1),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          "-${informatica[index].discount}%",
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      //botão favoritos
-                      const LikeButton(animationDuration: Duration(seconds: 0))
-                    ],
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => DetalhesPage(
+                    categoria: informatica,
+                    index: index,
+                    tagValor: tagImage,
                   ),
                 ),
-                 Expanded(
-                  child: HeroImage(
-                      tag: '$tagImage-$index', image: informatica[index].image!),
+              );
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  color: Color.fromRGBO(109, 68, 166, 1),
+                  width: 1,
                 ),
-                Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(informatica[index].title!,
-                        style: const TextStyle(
-                            fontSize: 15,
-                            color: Color.fromRGBO(109, 68, 166, 1),
-                            fontWeight: FontWeight.bold))),
-                Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 7,
+                    ),
                     child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("R\$${informatica[index].price}",
-                              style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color.fromRGBO(109, 68, 166, 1))),
-                          const Icon(Icons.shopping_cart_checkout,
-                              color: Color.fromRGBO(109, 68, 166, 1)),
-                        ])),
-              ]))
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(109, 68, 166, 1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            "-${informatica[index].discount}%",
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        // Botão favoritos
+                        FavoritosBotao(),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: HeroImage(
+                      tag: '$tagImage-$index',
+                      image: informatica[index].image!,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      informatica[index].title!,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Color.fromRGBO(109, 68, 166, 1),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 5,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "R\$${informatica[index].price}",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(109, 68, 166, 1),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.shopping_cart_checkout,
+                          color: Color.fromRGBO(109, 68, 166, 1),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
