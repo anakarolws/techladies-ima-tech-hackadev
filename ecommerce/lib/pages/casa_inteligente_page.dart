d480ed06c9174af524a5ac022d2e4804f8d30606
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-09-09 20:00:31 -0300

    Update casa_inteligente_page.dart

--- ecommerce/lib/pages/casa_inteligente_page.dart
+++ ecommerce/lib/pages/casa_inteligente_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/model/botao_favoritos.dart';
 import 'package:ecommerce/model/dados_produtos.dart';
 import 'package:ecommerce/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +8,7 @@ import '../widgets/hero_details.dart';
 import '../widgets/hero_image.dart';
 
 class CasaInteligentePrice extends StatefulWidget {
-  const CasaInteligentePrice({super.key});
+  const CasaInteligentePrice({Key? key});
 
   @override
   State<CasaInteligentePrice> createState() => _CasaInteligentePriceState();
@@ -28,74 +29,107 @@ class CasaInteligentePage extends StatelessWidget {
   Widget build(BuildContext context) {
     const String tagImage = 'hero-casainteligente';
 
-    DetalhesPage detalhesPage = DetalhesPage();
-
-    return Scaffold(
+   return Scaffold(
       appBar: CustomAppBar(title: titulo),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 0.78),
+          crossAxisCount: 2,
+          childAspectRatio: 0.78,
+        ),
         itemCount: casaInteligente.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-            onTap: () => detalhesPage.detalhesCategoria(context, casaInteligente, index, tagImage),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => DetalhesPage(
+                    categoria: casaInteligente,
+                    index: index,
+                    tagValor: tagImage,
+                  ),
+                ),
+              );
+            },
             child: Card(
               shape: RoundedRectangleBorder(
                 side: const BorderSide(
-                    color: Color.fromRGBO(109, 68, 166, 1), width: 1),
+                  color: Color.fromRGBO(109, 68, 166, 1),
+                  width: 1,
+                ),
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Column(children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 7,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
                             color: const Color.fromRGBO(109, 68, 166, 1),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          "-${casaInteligente[index].discount}%",
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            "-${casaInteligente[index].discount}%",
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      const LikeButton(animationDuration: Duration(seconds: 0))
-                    ],
+                        FavoritosBotao(),
+                      ],
+                    ),
                   ),
-                ),
-               Expanded(
-                  child: HeroImage(
-                      tag: '$tagImage-$index', image: casaInteligente[index].image!),
-                ),
-                Container(
+                  Expanded(
+                    child: HeroImage(
+                      tag: '$tagImage-$index',
+                      image: casaInteligente[index].image!,
+                    ),
+                  ),
+                  Container(
                     padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(casaInteligente[index].title!,
-                        style: const TextStyle(
-                            fontSize: 15,
-                            color: Color.fromRGBO(109, 68, 166, 1),
-                            fontWeight: FontWeight.bold))),
-                Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    child: Text(
+                      casaInteligente[index].title!,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Color.fromRGBO(109, 68, 166, 1),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 5,
+                    ),
                     child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("R\$${casaInteligente[index].price}",
-                              style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color.fromRGBO(109, 68, 166, 1))),
-                          const Icon(Icons.shopping_cart_checkout,
-                              color: Color.fromRGBO(109, 68, 166, 1)),
-                        ])),
-              ]))
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "R\$${casaInteligente[index].price}",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(109, 68, 166, 1),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.shopping_cart_checkout,
+                          color: Color.fromRGBO(109, 68, 166, 1),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
